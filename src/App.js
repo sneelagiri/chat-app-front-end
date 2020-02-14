d7bdd4709dc18e5eda24f671d5fd0cfcc50349ab
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-14 14:01:11 +0100

    show stream on page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
 import "./App.css";
 class App extends React.Component {
+  state = {
+    messages: []
+  };
+
   stream = new EventSource("http://localhost:4000/stream");
 
   componentDidMount() {
-    this.stream.onmessage = function(event) {
+    this.stream.onmessage = event => {
       // console.log("event test", event);
       const { data } = event;
       // console.log("data test", data);
       const parsed = JSON.parse(data);
       // console.log("parse test", parsed);
+      this.setState({ messages: parsed });
     };
   }
 
   render() {
-    return <div>Hello!</div>;
+    const paragraphs = this.state.messages.map(message => {
+      return <p key={message.id}>{message.text}</p>;
+    });
+    return <div>{paragraphs}</div>;
   }
 }
 
